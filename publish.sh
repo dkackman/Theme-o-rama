fba6b3bbe49a30380391bf8592d79ea58d3a7fb1
Don Kackman <dkackman@gmail.com>
2025-09-06 19:05:31 -0500

    not service correctly

--- publish.sh
+++ publish.sh
@@ -4,4 +4,10 @@ pnpm prettier
 pnpm run extract    
 pnpm run compile    
 pnpm run build:web
+
+# Clear docs directory and copy fresh build
+rm -rf docs/*
 cp -rf dist-web/* docs/
+echo "" > docs/.nojekyll
+
+echo "Build completed and copied to docs/"
