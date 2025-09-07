c2d7e4652beba252ffc77472cebe42f5c9ee0ca8
Don Kackman <dkackman@gmail.com>
2025-09-06 19:08:19 -0500

    not serving

--- publish.sh
+++ publish.sh
@@ -10,4 +10,10 @@ rm -rf docs/*
 cp -rf dist-web/* docs/
 echo "" > docs/.nojekyll
 
+# Add cache-busting headers to the copied index.html
+sed -i '' '/<title>Sage<\/title>/i\
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />\
+    <meta http-equiv="Pragma" content="no-cache" />\
+    <meta http-equiv="Expires" content="0" />' docs/index.html
+
 echo "Build completed and copied to docs/"
