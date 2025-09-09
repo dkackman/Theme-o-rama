4ffd42de3e7e2dda85a7f040d085161809fe60b6
Don Kackman <dkackman@gmail.com>
2025-09-09 15:29:03 -0500

    fix  theme loading race condition

--- clean-build.sh
+++ clean-build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Parse command line arguments
-AGGRESSIVE=false
-for arg in "$@"; do
-    case $arg in
-        --aggressive)
-            AGGRESSIVE=true
-            shift
-            ;;
-        *)
-            # Unknown option
-            ;;
-    esac
-done
-
 cd src-lib
 
 pnpm run clean
@@ -23,14 +9,7 @@ pnpm run build
 
 cd ..
 
-# If --aggressive flag is set, remove all node_modules directories
-if [ "$AGGRESSIVE" = true ]; then
-    echo "Aggressive mode: Removing all node_modules directories..."
-    rm -rf node_modules
-else
-    find . -name "node_modules" -type d -exec rm -rf {} + 2>/dev/null || true
-fi
-
+rm -rf node_modules
 pnpm run clean
 pnpm install
 pnpm prettier
